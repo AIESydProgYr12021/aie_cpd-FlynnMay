8b97fa7a945d120212d20402f3019c0d8e87e850
Flynn <Flynn@maytek.com.au>
2022-11-23 10:05:27 +1100

    Grid system improvements

---
+++ Project/Assets/Scripts/Managers/Grid/Grid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+[Serializable]
+public struct Grid
+{
+    private Dictionary<int, Dictionary<int, int>> gridMap;
+    [SerializeField] private int height;
+    [SerializeField] int rows;
+    [SerializeField] private int columns;
+    private List<GridObject> gridObjects;
+    public Dictionary<int, Dictionary<int, int>> GridMap { get => gridMap; set => gridMap = value; }
+    public int Height { get => height; set => height = value; }
+    public List<GridObject> GridObjects { get => gridObjects; set => gridObjects = value; }
+    public int Rows { get => rows; }
+    public int Columns { get => columns; }
+
+
+    public Grid(int height = 0, int rows = 0, int columns = 0, List<GridObject> gridObjects = null, Dictionary<int, Dictionary<int, int>> map = null)
+    {
+        this.height = height;
+        this.rows = rows;
+        this.columns = columns;
+        this.gridObjects = gridObjects;
+        gridMap = (map ?? new Dictionary<int, Dictionary<int, int>>());
+    }
+
+    public void ClearGrid()
+    {
+        GridMap = new Dictionary<int, Dictionary<int, int>>();
+        for (int z = 0; z < rows; z++)
+        {
+            GridMap[z] = new Dictionary<int, int>();
+            for (int x = 0; x < columns; x++)
+            {
+                GridMap[z][x] = 0;
+            }
+        }
+    }
+}
--- Project/Assets/Scripts/Managers/Grid/GridManager.cs
+++ Project/Assets/Scripts/Managers/Grid/GridManager.cs
@@ -4,46 +4,36 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    Dictionary<int, Dictionary<int, int>> gridFloor;
+    public Grid floorGrid;
     Dictionary<int, Dictionary<int, int>> gridProps;
     int rows, cols;
 
     private void Awake()
     {
         SetGrid();
-        LoadGrid();
+        LoadGrid(floorGrid);
     }
 
     private void SetGrid()
     {
-        gridFloor = new Dictionary<int, Dictionary<int, int>>();
-        gridProps = new Dictionary<int, Dictionary<int, int>>();
-        rows = 50;
-        cols = 50;
-        for (int z = 0; z < rows; z++)
-        {
-            gridFloor[z] = new Dictionary<int, int>();
-            for (int x = 0; x < cols; x++)
-            {
-                gridFloor[z][x] = Random.Range(0, 2);
-                gridProps[z][x] = Random.Range(0, 2);
-            }
-        }
+        floorGrid = new Grid(0, 50, 50);
+        floorGrid.ClearGrid();
     }
 
-    private void LoadGrid()
+    private void LoadGrid(Grid grid)
     {
         var pos = Vector3.zero;
-        List<GridObject> objects = GridObjectContainer.GetGridObjects();
+        List<GridObject> objects = grid.GridObjects;
+        pos.y = grid.Height;
 
-        for (int z = 0; z < rows; z++)
+        for (int z = 0; z < grid.Rows; z++)
         {
-            for (int x = 0; x < cols; x++)
+            for (int x = 0; x < grid.Columns; x++)
             {
                 pos.x = x;
                 pos.z = z;
 
-                int index = gridFloor[z][x] - 1;
+                int index = grid.GridMap[z][x] - 1;
                 if (index < 0)
                     continue;
 
@@ -55,11 +45,11 @@ public class GridManager : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        if (gridFloor == null)
+        if (floorGrid.GridMap == null)
             SetGrid();
 
         var pos = Vector3.zero;
-
+        pos.y = floorGrid.Height;
         for (int z = 0; z < rows; z++)
         {
             for (int x = 0; x < cols; x++)
@@ -67,7 +57,7 @@ public class GridManager : MonoBehaviour
                 pos.x = x;
                 pos.z = z;
 
-                Gizmos.color = gridFloor[z][x] == 0 ? Color.blue : Color.red;
+                Gizmos.color = floorGrid.GridMap[z][x] == 0 ? Color.blue : Color.red;
                 Gizmos.DrawWireCube(pos, new Vector3(1 - 0.05f, 0.01f, 1 - 0.05f));
             }
         }
